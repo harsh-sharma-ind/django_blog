ec897cc87ad56e817a169a62c3681994b8577e80
harsh-sharma-ind <harsh2000hs.hs@gmail.com>
2020-09-24 11:13:28 +0530

    Initial Commit

---
+++ theblog/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from .models import Post, Category, Comment
+
+choices = Category.objects.all().values_list('name', 'name')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet', 'header_image')
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your title'}),
+            'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
+            'author' : forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
+            #'author' : forms.Select(attrs={'class': 'form-control'}),
+            'category' : forms.Select(choices=choices, attrs={'class': 'form-control'}),
+            'body' : forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet' : forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body', 'snippet')
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your title'}),
+            'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
+            'body' : forms.Textarea(attrs={'class': 'form-control'}),
+            'snippet' : forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name',  'body')
+
+        widgets = {
+            'name' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your name'}),
+            
+            'body' : forms.Textarea(attrs={'class': 'form-control'}),
+            
+        }
